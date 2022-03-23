ce6a54749db62c13ff88ea010b9ce16006e090cf
Dworv YT <dwarvyt@gmail.com>
2022-03-23 09:04:11 -0700

    finished the check

--- app/exts/apply.py
+++ app/exts/apply.py
@@ -4,19 +4,20 @@ from interactions.ext.enhanced import *
 from interactions.ext.checks import check
 
 import const
-from checks import 
+from checks import can_apply
 
 class Apply(EnhancedExtension):
     """An extension dedicated to /apply."""
 
     @inter.extension_command()
-    @check
+    @check(can_apply)
     async def apply(
         self, 
         ctx: inter.CommandContext,
         url: EnhancedOption(str, 'The url to your application'),
     ):
         """Apply to become a member of the Universe Editing Team"""
+        await ctx.send('it worked')
 
         
 
