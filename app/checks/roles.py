3bbd23dd609a7c78b8e3a7f0560f0f73b25f1350
Dworv YT <dwarvyt@gmail.com>
2022-03-25 22:01:13 -0700

    change to build_embed name

--- app/checks/roles.py
+++ app/checks/roles.py
@@ -1,18 +1,18 @@
 
 import const
-from tools import build_response_embed
+from tools import build_embed
 
 async def can_apply(ctx):
     """
     Check if the user can apply.
     """
     if const.METADATA['role']['member'] in ctx.author.roles:
-        await ctx.send(embeds=build_response_embed('To apply for Member+, you need to ask the staff so they can promote you.'))
+        await ctx.send(embeds=build_embed('To apply for Member+, you need to ask the staff so they can promote you.'))
         return False
     if const.METADATA['role']['memberplus'] in ctx.author.roles:
-        await ctx.send(embeds=build_response_embed('You have already reached the highest level of editor.'))
+        await ctx.send(embeds=build_embed('You have already reached the highest level of editor.'))
         return False
     if const.METADATA['role']['reviewer'] in ctx.author.roles:
-        await ctx.send(embeds=build_response_embed('Reviewers are not allowed to apply'))
+        await ctx.send(embeds=build_embed('Reviewers are not allowed to apply'))
         return False
     return True
